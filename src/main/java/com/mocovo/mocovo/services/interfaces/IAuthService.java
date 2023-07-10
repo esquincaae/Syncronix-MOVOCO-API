0610e973429042fd32f20bec01f249baac0021a6
esquincaae <97558097+esquincaae@users.noreply.github.com>
2023-07-10 13:09:28 -0600

    delete JWT

--- src/main/java/com/mocovo/mocovo/services/interfaces/IAuthService.java
+++
@@ -1,8 +0,0 @@
-package com.mocovo.mocovo.services.interfaces;
-
-import com.mocovo.mocovo.controllers.dto.requests.AuthenticationRequest;
-import com.mocovo.mocovo.controllers.dto.responses.BaseResponse;
-
-public interface IAuthService {
-    BaseResponse authenticate(AuthenticationRequest request);
-}
